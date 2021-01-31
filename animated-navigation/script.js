87e9b434b77668148db294a00725ff5a153fc9b1
Kalpana Sundar <74096890+KalpanaAmmu@users.noreply.github.com>
2021-01-31 22:20:10 +0530

    Add Another Project

---
+++ animated-navigation/script.js
@@ -0,0 +1,12 @@
+const toggle=document.querySelector("#toggle");
+const nav=document.querySelector("#nav")
+toggle.addEventListener('click',function(){
+    nav.classList.toggle('active')
+});
+
+
+
+// const toggle = document.getElementById('toggle')
+// const nav = document.getElementById('nav')
+
+// toggle.addEventListener('click', () => nav.classList.toggle('active'))
